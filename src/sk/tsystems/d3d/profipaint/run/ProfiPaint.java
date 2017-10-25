040dd9a0904ce37f3f01b7b0e6e05f46304789a4
student <student@DESKTOP-DKDBLTK>
2017-10-25 15:05:50 +0200

    InitialCommit

---
+++ src/sk/tsystems/d3d/profipaint/run/ProfiPaint.java
@@ -0,0 +1,12 @@
+package sk.tsystems.d3d.profipaint.run;
+
+import javax.swing.JFrame;
+
+public class ProfiPaint extends JFrame {
+	private static final long serialVersionUID = 6639228443591986333L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
