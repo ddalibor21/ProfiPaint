10c8dc0467acb41e942ee985d073891b6de93099
student <student@DESKTOP-DKDBLTK>
2017-10-26 11:04:17 +0200

    Paint panel & paint Menu

---
+++ src/sk/tsystems/d3d/profipaint/editor/PaintPanel.java
@@ -0,0 +1,38 @@
+package sk.tsystems.d3d.profipaint.editor;
+
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JToolBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JMenu;
+
+public class PaintPanel extends JPanel{
+	public PaintPanel() {
+		
+		
+//		setBackground(Color.RED);
+//		setLayout(new BorderLayout(0, 0));
+//		
+//		JToolBar toolBar = new JToolBar();
+//		add(toolBar, BorderLayout.NORTH);
+//		
+//		JButton btnSelect = new JButton("Select");
+//		toolBar.add(btnSelect);
+//		
+//		JButton btnRotate = new JButton("Rotate");
+//		toolBar.add(btnRotate);
+//		
+//		JButton btnResize = new JButton("Resize");
+//		toolBar.add(btnResize);
+		
+		
+	}
+
+}
