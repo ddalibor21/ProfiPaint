1d5399dc6015b7a58437d37493e10bde0161645e
student <student@DESKTOP-DKDBLTK>
2017-10-26 10:44:14 +0200

    Add save and open file

---
+++ src/sk/tsystems/d3d/profipaint/filesupport/MyException.java
@@ -0,0 +1,11 @@
+package sk.tsystems.d3d.profipaint.filesupport;
+
+public class MyException extends RuntimeException {
+
+	private static final long serialVersionUID = -3050493915041764669L;
+
+	public MyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
