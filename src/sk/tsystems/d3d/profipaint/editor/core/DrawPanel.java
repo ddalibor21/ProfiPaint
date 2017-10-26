45f00fe0e615515d122e365116548fa3fe64f8bc
student <student@DESKTOP-DKDBLTK>
2017-10-26 13:33:36 +0200

    Select graphics, Move, Basic resize

--- src/sk/tsystems/d3d/profipaint/editor/core/DrawPanel.java
+++ src/sk/tsystems/d3d/profipaint/editor/core/DrawPanel.java
@@ -4,8 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +22,10 @@ public class DrawPanel extends MouseInteractionPanel implements DrawFace {
 	private List<Geometric> geometrics;
 	private List<Geometric> selection;
 	private Geometric newGeo;
+	private Geometric selected;
+	private double pdX;
+	private double pdY;
+	private Shape sizer;
 
 	public DrawPanel(int width, int height) {
 		super();
@@ -39,12 +46,21 @@ public class DrawPanel extends MouseInteractionPanel implements DrawFace {
 
 	@Override
 	public void paint(Graphics g) {
-		// super.paint(g);
 		draw(Graphics2D.class.cast(g));
 	}
 
 	private void draw(Graphics2D g) {
 		Drawer.draw(geometrics, g, this, Color.white);
+		if (selected != null) {
+			Rectangle2D rectSel = new Rectangle2D.Double(selected.getPosition().getX(), selected.getPosition().getY(),
+					selected.getWidth(), selected.getHeight());
+			g.setColor(Color.red);
+			g.draw(rectSel);
+			sizer = new Rectangle2D.Double(selected.getPosition().getX() + selected.getWidth() - 3,
+					selected.getPosition().getY() + selected.getHeight() - 3, 6, 6);
+
+			g.fill(sizer);
+		}
 	}
 
 	private void cancelNewGeo() {
@@ -80,22 +96,63 @@ public class DrawPanel extends MouseInteractionPanel implements DrawFace {
 		if (isNewGeo()) {
 			newGeo.setPosition(pt);
 			repaint();
+			return;
 		}
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// add new geo into paint 
+		// add new geo into paint
 		if (isNewGeo() && e.getClickCount() == 1) {
 			if (e.getButton() == MouseEvent.BUTTON3)
 				cancelNewGeo();
 
 			newGeoAdded();
-			
+
 			repaint();
+			return;
+		}
+
+		// select geo
+		selected = null;
+		for (Geometric ge : geometrics) {
+			if (ge.getPosition().x <= e.getX() && ge.getPosition().x + ge.getWidth() >= e.getX()
+					&& ge.getPosition().y <= e.getY() && ge.getPosition().y + ge.getHeight() >= e.getY()) {
+				selected = ge;
+				repaint();
+				break;
+			}
+		}
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if(selected!=null && sizer.contains(e.getPoint())) {
+			selected.setWidth(e.getX()-selected.getPosition().getX());
+			selected.setHeight(e.getY()-selected.getPosition().getY());
+			repaint();
+			return;
 		}
 		
-		// select geo
+		if (selected != null) {
+			if (pdX < 0) {
+				pdX = e.getX() - selected.getPosition().getX();
+				pdY = e.getY() - selected.getPosition().getY();
+			}
+			selected.setPosition(new Point2D.Double(e.getX() - pdX, e.getY() - pdY));
+			repaint();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		pdX = -1;
+		pdY = -1;
+	}
+
+	private Point2D.Double p2p(Point p) {
+		return new Point2D.Double(p.getX(), p.getY());
 	}
 
 }
